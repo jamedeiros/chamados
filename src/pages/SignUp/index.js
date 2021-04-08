0fcdfd8af7c59f80e56b2ae78a46182af5fd6f39
José Augusto <jamedeiros@gmail.com>
2021-04-08 10:55:05 -0300

    Adding singup page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,52 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png';
+
 function SignUp() {
-    return (
-      <div>
-        <h1>TELA DE CADASTRO</h1>
-      </div>
-    );
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert('CLICOU');
   }
+
+  return (
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={ logo } alt="Sistema Logo" />
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <h1>Cadastrar uma conta</h1>
+          <input 
+            type="text" 
+            placeholder="Seu nome" 
+            value={ nome }
+            onChange={ (e) => setNome(e.target.value) }
+          />
+          <input 
+            type="text" 
+            placeholder="email@email.com" 
+            value={ email } 
+            onChange={ (e) => setEmail(e.target.value) }
+          />
+          <input 
+            type="password" 
+            placeholder="********" 
+            value={ password }
+            onChange={ (e) => setPassword(e.target.value) }
+          />
+          <button type="submit">Cadastrar</button>
+        </form>
+
+        <Link to="/">Já tem uma conta entre</Link>
+      </div>
+    </div>
+  );
+}
   
-  export default SignUp;
+export default SignUp;
   
